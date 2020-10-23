b0d108b8e435c6e2e99f60c641db5e92ef990ffd
Valdimiro francisco <valdimiroeinstein@gmail.com>
2020-10-23 14:15:48 +0200

    company form inicializado

--- resources/views/provider/company/index.blade.php
+++ resources/views/provider/company/index.blade.php
@@ -3,6 +3,10 @@
     Company
 @endsection
 
+@section('meta')
+    <link href="{{ asset('assets/plugins/x-editable/dist/bootstrap3-editable/css/bootstrap-editable.css') }}" rel="stylesheet">
+@endsection
+
 
 @section('content')
     <div class="page-wrapper">
@@ -58,55 +62,23 @@
                                     <div class="card-body">
                                         <div class="profiletimeline">
                                             <div class="sl-item">
-                                                <div class="sl-left"> <img src="../assets/images/users/1.jpg" alt="user" class="img-circle"> </div>
-                                                <div class="sl-right">
-                                                    <div><a href="#" class="link">John Doe</a> <span class="sl-date">5 minutes ago</span>
-                                                        <p>assign a new task <a href="#"> Design weblayout</a></p>
-                                                        <div class="row">
-                                                            <div class="col-lg-3 col-md-6 m-b-20"><img src="../assets/images/big/img1.jpg" class="img-responsive radius"></div>
-                                                            <div class="col-lg-3 col-md-6 m-b-20"><img src="../assets/images/big/img2.jpg" class="img-responsive radius"></div>
-                                                            <div class="col-lg-3 col-md-6 m-b-20"><img src="../assets/images/big/img3.jpg" class="img-responsive radius"></div>
-                                                            <div class="col-lg-3 col-md-6 m-b-20"><img src="../assets/images/big/img4.jpg" class="img-responsive radius"></div>
-                                                        </div>
-                                                        <div class="like-comm"> <a href="javascript:void(0)" class="link m-r-10">2 comment</a> <a href="javascript:void(0)" class="link m-r-10"><i class="fa fa-heart text-danger"></i> 5 Love</a> </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="sl-item">
-                                                <div class="sl-left"> <img src="../assets/images/users/2.jpg" alt="user" class="img-circle"> </div>
-                                                <div class="sl-right">
-                                                    <div> <a href="#" class="link">John Doe</a> <span class="sl-date">5 minutes ago</span>
-                                                        <div class="m-t-20 row">
-                                                            <div class="col-md-3 col-xs-12"><img src="../assets/images/big/img1.jpg" alt="user" class="img-responsive radius"></div>
-                                                            <div class="col-md-9 col-xs-12">
-                                                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. </p> <a href="#" class="btn btn-success"> Design weblayout</a></div>
-                                                        </div>
-                                                        <div class="like-comm m-t-20"> <a href="javascript:void(0)" class="link m-r-10">2 comment</a> <a href="javascript:void(0)" class="link m-r-10"><i class="fa fa-heart text-danger"></i> 5 Love</a> </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="sl-item">
-                                                <div class="sl-left"> <img src="../assets/images/users/3.jpg" alt="user" class="img-circle"> </div>
-                                                <div class="sl-right">
-                                                    <div><a href="#" class="link">John Doe</a> <span class="sl-date">5 minutes ago</span>
-                                                        <p class="m-t-10"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper </p>
-                                                    </div>
-                                                    <div class="like-comm m-t-20"> <a href="javascript:void(0)" class="link m-r-10">2 comment</a> <a href="javascript:void(0)" class="link m-r-10"><i class="fa fa-heart text-danger"></i> 5 Love</a> </div>
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="sl-item">
                                                 <div class="sl-left"> <img src="../assets/images/users/4.jpg" alt="user" class="img-circle"> </div>
                                                 <div class="sl-right">
-                                                    <div><a href="#" class="link">John Doe</a> <span class="sl-date">5 minutes ago</span>
+                                                    <div><a href="#" class="link">Company Name</a>
                                                         <blockquote class="m-t-10">
-                                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                                                            <a href="#" id="inline-name" data-type="text" data-pk="1" data-title="Enter company name" class="editable editable-click" style="">Company Name</a>
+                                                        </blockquote>
+                                                    </div>
+                                                </div>
+                                                <div class="sl-right">
+                                                    <div><a href="#" class="link">Company Slogan</a>
+                                                        <blockquote class="m-t-10">
+                                                            <a href="#" id="inline-slogan" data-type="text" data-pk="1" data-title="Enter company slogan" class="editable editable-click" style="">Company Slogan</a>
                                                         </blockquote>
                                                     </div>
                                                 </div>
                                             </div>
+                                            <hr>
                                         </div>
                                     </div>
                                 </div>
@@ -220,4 +192,131 @@
     <\div>
 @endsection
 
+@section('scripts')
+<script src="{{ asset('assets/plugins/moment/moment.js')}}" ></script>
+<script src="{{ asset('assets/plugins/x-editable/dist/bootstrap3-editable/js/bootstrap-editable.js')}}" ></script>
+<script type="text/javascript">
+    $(function() {
+        //editables 
+        $('#username').editable({
+            type: 'text',
+            pk: 1,
+            name: 'username',
+            title: 'Enter username'
+        });
+
+        $('#firstname').editable({
+            validate: function(value) {
+                if ($.trim(value) == '') return 'This field is required';
+            }
+        });
+
+        $('#sex').editable({
+            prepend: "not selected",
+            source: [{
+                value: 1,
+                text: 'Male'
+            }, {
+                value: 2,
+                text: 'Female'
+            }],
+            display: function(value, sourceData) {
+                var colors = {
+                        "": "#98a6ad",
+                        1: "#5fbeaa",
+                        2: "#5d9cec"
+                    },
+                    elem = $.grep(sourceData, function(o) {
+                        return o.value == value;
+                    });
+
+                if (elem.length) {
+                    $(this).text(elem[0].text).css("color", colors[value]);
+                } else {
+                    $(this).empty();
+                }
+            }
+        });
+
+        $('#status').editable();
+
+        $('#group').editable({
+            showbuttons: false
+        });
+
+        $('#dob').editable();
+
+        $('#comments').editable({
+            showbuttons: 'bottom'
+        });
+
+        //inline
+
+
+        $('#inline-name').editable({
+            type: 'text',
+            pk: 1,
+            name: 'username',
+            title: 'Enter username',
+            mode: 'inline'
+        });
+
+        $('#inline-slogan').editable({
+            validate: function(value) {
+                if ($.trim(value) == '') return 'This field is required';
+            },
+            mode: 'inline'
+        });
+
+        $('#inline-sex').editable({
+            prepend: "not selected",
+            mode: 'inline',
+            source: [{
+                value: 1,
+                text: 'Male'
+            }, {
+                value: 2,
+                text: 'Female'
+            }],
+            display: function(value, sourceData) {
+                var colors = {
+                        "": "#98a6ad",
+                        1: "#5fbeaa",
+                        2: "#5d9cec"
+                    },
+                    elem = $.grep(sourceData, function(o) {
+                        return o.value == value;
+                    });
+
+                if (elem.length) {
+                    $(this).text(elem[0].text).css("color", colors[value]);
+                } else {
+                    $(this).empty();
+                }
+            }
+        });
+
+        $('#inline-status').editable({
+            mode: 'inline'
+        });
+
+        $('#inline-group').editable({
+            showbuttons: false,
+            mode: 'inline'
+        });
+
+        $('#inline-dob').editable({
+            mode: 'inline'
+        });
+
+        $('#inline-comments').editable({
+            showbuttons: 'bottom',
+            mode: 'inline'
+        });
+
+
+
+    });
+    </script>
+@endsection
 
